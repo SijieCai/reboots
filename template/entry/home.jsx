5b28895629c97b7556026339f8e3ee9bd855201f
caisijie <caisijie@360.cn>
2016-06-15 11:26:40 +0800

    fix bugs and update version

--- template/entry/home.jsx
+++ template/entry/home.jsx
@@ -2,20 +2,26 @@ import {Component} from 'react';
 import './style.css';
 import LeftMenu from '<%=name%>/components/left-menu';
 import TopMenu from '<%=name%>/components/top-menu';
+import {RBLayout, RBAlert} from 'rb-component';
 
 export default class extends Component {
   state = {menus: []};
   render() { 
     return (
-      <div className="app-container">
+      <RBLayout>
+        <RBAlert/>
+        <div className="rb-layout__top">
         <TopMenu {...this.props} menus={this.state.menus}/>
-        <div className="app__content">
-          <div className="main">
+        </div>
+        <div className="rb-layout__bottom">
+          <div className="rb-layout__bottom__left">
             <LeftMenu menuGroups={this.state.subMenus}/>
+          </div>
+          <div className="rb-layout__content">
             {this.props.children}
           </div>
         </div>
-      </div>
+      </RBLayout>
     );
   }
 };
